4a1ec9db6543bd41b74d08fa7e715e9b686c3cb2
juliamagenta <juliazaninistein@gmail.com>
2026-09-22 17:03:05 -0300

    Update exercicio2.js

--- exercicio2.js
+++ exercicio2.js
@@ -1,1 +1,17 @@
+//2. While – Contagem Regressiva com Condições//
 
+let numero = 30;
+
+while (numero >= 1) {
+    if (numero % 3 === 0 && numero % 5 === 0) {
+        console.log(numero + " - PingPong");
+    } else if (numero % 3 === 0) {
+        console.log(numero + " - Ping");
+    } else if (numero % 5 === 0) {
+        console.log(numero + " - Pong");
+    } else {
+        console.log(numero);
+    }
+
+    numero--;
+}
