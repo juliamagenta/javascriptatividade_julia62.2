8a8b7205e5a4179cd0a0cf0fe5a4cda2b9fa25c2
juliamagenta <juliazaninistein@gmail.com>
2026-09-22 17:06:18 -0300

    Update exercicio7.js

--- exercicio7.js
+++ exercicio7.js
@@ -1,1 +1,14 @@
+//7. While – Soma Acumulada com Condições//
 
+let numero = 1;
+let soma = 0;
+
+while (numero <= 20) {
+    if (numero % 2 === 0 && numero % 4 !== 0) {
+        soma += numero;
+    }
+
+    numero++;
+}
+
+console.log("Resultado acumulado: " + soma);
