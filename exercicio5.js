fb7a669f50abe869124ba54a41f1582f9b898103
juliamagenta <juliazaninistein@gmail.com>
2026-09-22 17:05:33 -0300

    Update exercicio5.js

--- exercicio5.js
+++ exercicio5.js
@@ -1,1 +1,23 @@
+//5. Break – Múltiplo de 7 Avançado//
+
+for (let numero = 1; numero <= 100; numero++) {
+    let primo = true;
+
+    if (numero < 2) {
+        primo = false;
+    }
+
+    for (let divisor = 2; divisor < numero; divisor++) {
+        if (numero % divisor === 0) {
+            primo = false;
+            break;
+        }
+    }
+
+    if (numero % 7 === 0 && primo) {
+        console.log("Primeiro múltiplo de 7 que é primo: " + numero);
+        break;
+    }
+}
+
 
