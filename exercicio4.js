ed864fc1e830c2c5fef96c0c65112852fca42974
juliamagenta <juliazaninistein@gmail.com>
2026-09-22 17:03:58 -0300

    Update exercicio4.js

--- exercicio4.js
+++ exercicio4.js
@@ -1,1 +1,15 @@
+//4. For – Tabuada Filtrada//
 
+let numero = Number(prompt("Digite um número de 1 a 10:"));
+let soma = 0;
+
+for (let i = 1; i <= 10; i++) {
+    let resultado = numero * i;
+
+    if (resultado % 4 === 0) {
+        console.log(numero + " x " + i + " = " + resultado);
+        soma += resultado;
+    }
+}
+
+console.log("Soma dos resultados múltiplos de 4: " + soma);
