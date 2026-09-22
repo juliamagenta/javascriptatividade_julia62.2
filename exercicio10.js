1af5228f0e92ec11e991c6db3c9888d999afec20
juliamagenta <juliazaninistein@gmail.com>
2026-09-22 17:07:38 -0300

    Update exercicio10.js

--- exercicio10.js
+++ exercicio10.js
@@ -1,1 +1,53 @@
+//10. Switch + Loop – Meses Avançados//
 
+for (let mes = 1; mes <= 12; mes++) {
+    switch (mes) {
+        case 1:
+            console.log("Janeiro");
+            break;
+
+        case 2:
+            console.log("Fevereiro - 28 ou 29 dias");
+            break;
+
+        case 3:
+            console.log("Março");
+            break;
+
+        case 4:
+            console.log("Abril");
+            break;
+
+        case 5:
+            console.log("Maio - Mês de provas");
+            break;
+
+        case 6:
+            console.log("Junho");
+            break;
+
+        case 7:
+            console.log("Julho - Férias");
+            break;
+
+        case 8:
+            console.log("Agosto - Mês de provas");
+            break;
+
+        case 9:
+            console.log("Setembro");
+            break;
+
+        case 10:
+            console.log("Outubro - Mês de provas");
+            break;
+
+        case 11:
+            console.log("Novembro");
+            break;
+
+        case 12:
+            console.log("Dezembro - Férias");
+            break;
+    }
+}
