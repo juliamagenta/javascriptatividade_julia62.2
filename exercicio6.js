ec7b7359e9a4962bc2308c9a0fadebcc9c0271c2
juliamagenta <juliazaninistein@gmail.com>
2026-09-22 17:05:53 -0300

    Update exercicio6.js

--- exercicio6.js
+++ exercicio6.js
@@ -1,1 +1,12 @@
+//6. Continue – Filtrando Ímpares//
+for (let numero = 1; numero <= 50; numero++) {
+    if (numero % 2 === 0) {
+        continue;
+    }
 
+    if (numero % 3 === 0 || numero % 7 === 0) {
+        continue;
+    }
+
+    console.log(numero);
+}
