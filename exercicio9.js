730110c268674a6efc16bb210d194a3276011a09
juliamagenta <juliazaninistein@gmail.com>
2026-09-22 17:07:14 -0300

    Update exercicio9.js

--- exercicio9.js
+++ exercicio9.js
@@ -1,1 +1,8 @@
+//9. For – Quadrado e Cubo//
 
+for (let numero = 2; numero <= 30; numero += 2) {
+    let quadrado = numero ** 2;
+    let cubo = numero ** 3;
+
+    console.log(numero + "² = " + quadrado + " | " + numero + "³ = " + cubo);
+}
