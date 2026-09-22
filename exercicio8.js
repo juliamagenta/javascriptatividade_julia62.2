2fbcb19ff6ec18094dc3b7b90a0c337ba4111689
juliamagenta <juliazaninistein@gmail.com>
2026-09-22 17:06:44 -0300

    Update exercicio8.js

--- exercicio8.js
+++ exercicio8.js
@@ -1,1 +1,24 @@
+//8. Do...while – Número Válido e Sequência//
 
+let numero;
+
+do {
+    numero = Number(prompt("Digite um número maior que 0:"));
+} while (numero <= 0 || isNaN(numero));
+
+console.log("Números primos até " + numero + ":");
+
+for (let i = 2; i <= numero; i++) {
+    let primo = true;
+
+    for (let divisor = 2; divisor < i; divisor++) {
+        if (i % divisor === 0) {
+            primo = false;
+            break;
+        }
+    }
+
+    if (primo) {
+        console.log(i);
+    }
+}
