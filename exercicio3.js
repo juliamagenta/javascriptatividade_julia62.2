f3f7a81cc2fe533e10f0ace55ffeb20591ee4040
juliamagenta <juliazaninistein@gmail.com>
2026-09-22 17:03:36 -0300

    Update exercicio3.js

--- exercicio3.js
+++ exercicio3.js
@@ -1,1 +1,24 @@
+//3. Do...while – Sistema de Login//
+let tentativas = 0;
+let login;
+let senha;
+let acesso = false;
 
+do {
+    login = prompt("Digite o login:");
+    senha = prompt("Digite a senha:");
+
+    tentativas++;
+
+    if (login === "admin" && senha === "1234") {
+        acesso = true;
+        console.log("Acesso liberado");
+    } else {
+        console.log("Login ou senha incorretos.");
+    }
+
+} while (!acesso && tentativas < 3);
+
+if (!acesso) {
+    console.log("Conta bloqueada");
+}
